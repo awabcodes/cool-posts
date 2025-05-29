d5e9d5580d2419ef46c56b752b4a97a9e262d9e1
awabcodes <awabcodes@gmail.com>
2025-05-29 14:59:29 +0400

    chore: setup eslint linting, prettier formatting and husky

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  preset: "jest-preset-angular",
-  testEnvironment: "jsdom",
-  setupFilesAfterEnv: ["<rootDir>/setup-jest.ts"],
-  moduleFileExtensions: ["ts", "js", "html"],
-  transformIgnorePatterns: ["node_modules/(?!.*\\.mjs$)"],
+  preset: 'jest-preset-angular',
+  testEnvironment: 'jsdom',
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  transformIgnorePatterns: ['node_modules/(?!.*\\.mjs$)'],
   collectCoverage: true,
-  coverageReporters: ["html", "text-summary"],
-  testMatch: ["**/+(*.)+(spec).+(ts)"],
+  coverageReporters: ['html', 'text-summary'],
+  testMatch: ['**/+(*.)+(spec).+(ts)'],
 };
